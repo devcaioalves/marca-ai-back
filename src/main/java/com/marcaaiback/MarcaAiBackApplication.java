b988f2e4131d4c51fa7cb97614cb14c969a63247
Caio Alves <caio.silvaalves2002@gmail.com>
2026-04-01 14:58:39 -0300

    add folders spring

---
+++ src/main/java/com/marcaaiback/MarcaAiBackApplication.java
@@ -0,0 +1,13 @@
+package com.marcaaiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcaAiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcaAiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marcaaiback/MarcaAiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcaaiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcaAiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
